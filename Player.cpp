744b94a0a875b829643c958c30bce03b4cf5b134
hun1001 <l01022899266@gmail.com>
2022-04-30 15:24:07 +0900

    파일 분할 버그 수정 아직

---
+++ Player.cpp
@@ -0,0 +1,7 @@
+#include "Player.h"
+
+Player::Player(long x, long y)
+{
+	_playerCurPos.x = x;
+	_playerCurPos.y = y;
+}
---
+++ Player.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "console.h"
+
+class Player
+{
+private:
+	POINT _playerCurPos;
+public:
+	Player(long x, long y);
+};
+
